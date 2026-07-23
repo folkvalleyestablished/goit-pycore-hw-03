6737feba6388804142a5cacb6d4f302421a03eb2
folkvalleyestablished <onethree2223@gmail.com>
2026-07-23 14:09:17 +0200

    Update task2.py

--- task2.py
+++ task2.py
@@ -2,17 +2,16 @@ import random
 
 
 def get_numbers_ticket(min, max, quantity):
-    if min < 1 or max > 1000:
-        return []
-
-    if quantity > max - min + 1:
+    if (
+        min < 1
+        or max > 1000
+        or min > max
+        or quantity < 0
+        or quantity > max - min + 1
+    ):
         return []
 
     numbers = random.sample(range(min, max + 1), quantity)
     numbers.sort()
 
     return numbers
-
-
-print(get_numbers_ticket(1, 49, 6))
-print(get_numbers_ticket(1, 5, 8))
